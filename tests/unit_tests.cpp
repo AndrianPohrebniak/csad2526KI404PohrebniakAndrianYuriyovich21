32afb1b6b4c85a30bb24bf47daaf10a9ef6451de
AndrianPohrebniak <pogra29@gmail.com>
2025-10-20 23:53:29 +0300

    feat: done 3 stage

---
+++ tests/unit_tests.cpp
@@ -0,0 +1,21 @@
+#include "math_operations.h"
+#include <cassert>
+#include <iostream>
+
+void test_add()
+{
+    // Test cases
+    assert(add(2, 3) == 5);
+    assert(add(-1, 1) == 0);
+    assert(add(0, 0) == 0);
+    assert(add(-5, -5) == -10);
+    assert(add(100, 200) == 300);
+
+    std::cout << "All tests passed!" << std::endl;
+}
+
+int main()
+{
+    test_add();
+    return 0;
+}
